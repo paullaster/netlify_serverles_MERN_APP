019094ef1c4e4dfb38d42d4195e695af3ffd9346
paullaster <paullasterokoth98@gmail.com>
2022-10-07 17:42:36 +0300

    cleint promise

--- functions/getMovies/getMovies.js
+++ functions/getMovies/getMovies.js
@@ -5,3 +5,8 @@ const mongodbClient = new MongoClient (process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const clientPromise = mongodbClient.connect ();
+
+const handler = async (event, context) => {
+   
+};
