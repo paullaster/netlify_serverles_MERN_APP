03a57082bbfb87177cae7f98cc0b147c2b1ae8dc
paullaster <paullasterokoth98@gmail.com>
2022-10-07 17:46:53 +0300

    db collection

--- functions/getMovies/getMovies.js
+++ functions/getMovies/getMovies.js
@@ -8,5 +8,13 @@ const mongodbClient = new MongoClient (process.env.MONGODB_URI, {
 const clientPromise = mongodbClient.connect ();
 
 const handler = async (event, context) => {
-   
+   try {
+    const database = (await clientPromise).db (process.env.MONGODB_DB);
+    const collection = await database.collection (process.env.MONGODB_COLLECTION);
+   } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify ({error: error.message}),
+    };
+  }
 };
